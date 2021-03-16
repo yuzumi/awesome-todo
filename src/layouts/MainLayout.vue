0ac8ca7949fae1433a15bf84058c7d10321da8dd
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2021-03-16 10:43:34 +0200

    feat(layouts.main): add navigation bars

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -12,10 +12,8 @@
         />
 
         <q-toolbar-title>
-          Quasar App
+          Awesome Todo
         </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
@@ -24,84 +22,82 @@
       show-if-above
       bordered
       content-class="bg-grey-1"
+      breakpoint="767"
     >
       <q-list>
         <q-item-label
           header
           class="text-grey-8"
         >
-          Essential Links
+          Navigation
         </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
+
+        <q-item
+          v-for="link of links"
+          :key="link.label"
+          :to="link.to"
+          clickable
+          exact
+          active-class="text-primary"
+        >
+          <q-item-section avatar>
+            <q-icon :name="link.icon" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>
+              {{ link.label }}
+            </q-item-label>
+          </q-item-section>
+        </q-item>
       </q-list>
     </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <q-footer>
+      <q-tabs>
+        <q-route-tab
+          v-for="link of links"
+          :key="link.label"
+          :to="link.to"
+          :icon="link.icon"
+          :label="link.label"
+          exact
+        />
+      </q-tabs>
+    </q-footer>
   </q-layout>
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue';
-
-const linksData = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev',
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework',
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev',
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev',
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev',
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev',
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev',
-  },
-];
-
 export default {
   name: 'MainLayout',
-  components: { EssentialLink },
   data() {
     return {
       leftDrawerOpen: false,
-      essentialLinks: linksData,
+      links: [
+        {
+          to: '/',
+          icon: 'eva-list-outline',
+          label: 'Todo',
+        },
+        {
+          to: '/settings',
+          icon: 'eva-settings-2-outline',
+          label: 'Settings',
+        },
+      ],
     };
   },
 };
 </script>
+
+<style lang="scss" scoped>
+@media screen and (min-width: 768px) {
+  .q-footer {
+    display: none;
+  }
+}
+</style>
